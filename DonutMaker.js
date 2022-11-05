c4015509372481d76dc633e0f267c5bf123f0a4f
cheyenneabel <113124769+cheyenneabel@users.noreply.github.com>
2022-11-05 14:28:20 -0400

    deleted container, added p tags with autoclicker count and cost, updated links to include github page and portfolio

--- DonutMaker.js
+++ DonutMaker.js
@@ -7,33 +7,43 @@ document.getElementById("alert").addEventListener("click", some_action);
 
 
 let donuts = 0;
-const container = (document.querySelector('.container').innerText =
-  'mmmmmmmmm doughnuts.... ahhhhhhh!') ;
 
 var donutCount = document.getElementById("counter");
 document.getElementById("alert").addEventListener("click", makeDonut);
+donutCount.innerHTML = donuts;
 
 function makeDonut() {
   donuts += 1;
   console.log(donuts)
-  donutCount.innerHTML = donuts
+  donutCount.innerHTML = donuts;
 }
 
 // autoclicker functions
 let autoClicker = 0;
+let oldCost = 100;
+let newCost = oldCost + (oldCost * .1)
 
 document.getElementById("autoClick").addEventListener("click", activateAutoClicker);
+var clickerCount = document.getElementById("autoClickerCount");
+var clickerCost = document.getElementById("autoClickerCost");
+clickerCount.innerHTML = autoClicker;
+clickerCost.innerHTML = oldCost;
 
 
 function activateAutoClicker() {
-  if (autoClicker < 1 && donuts >= 100) {
-    donuts -= 100;
-    autoClicker ++;
+  if (autoClicker == 0 && donuts >= 100)  {
+    donuts -= oldCost;
+  } 
+  if (autoClicker == 1 && donuts >= newCost) {
+    donuts -= newCost;
   }
+  if(autoClicker >= 2 && donuts >= newCost) {
+    donuts -= newCost;
+  }
+
+  autoClicker += 1;
   console.log(autoClicker, donuts)
-
   donutCount.innerHTML = donuts; 
-
 }
 
 
@@ -55,8 +65,10 @@ document.getElementById("alert").addEventListener("click", donutMultiplication);
 
 function buyDonutMultiplier() {
   if (donutMultiplier > 0) {
-    donutMultiplierCount = 10
+    donuts -=10;
   }
+  donutMultiplier++;
+  donutCount.innerHTML = donuts;
 }
 
 //function buyDonutMultiplier() {
@@ -127,30 +139,6 @@ Object.defineProperty(donutMultiplier, "subtract", {
   set: function (value) {this.donutMultiplierCount -= value;}
 });
 
-// purchase items **if no autoclickers, cost is 100. after, cost increases by 10%
 
-function purchaseAutoClicker() {
-  if (this.donutCount < 100) {
-    alert("You don't have enough donuts to purchase an AutoClicker")
-  } else if (donut.donutCount >= 100 && autoClicker.autoClickerCount == 0) {
-  donut.subtract = 100;
-  autoClicker.increment; }}
-  
 
-// user interaction
-var alertButton = document.getElementById('alert');
-alertButton.onclick = function addDonut() {
-  alert("+1 donut");
-  donut.increment;
-}
-
-var countNumber= document.getElementById('counter');
-countNumber.onclick = function checkDonutCount() {
-  alert("You have " + countNumber + " donuts. Wow!");
-}
-
-var autoClickButton= document.getElementById('autoClick');
-autoClickButton.onclick = function purchaseAutoClicker() {
-  alert("You purchased an AutoClicker with 100 of your donuts. Let's get to work!")
-}
 */
